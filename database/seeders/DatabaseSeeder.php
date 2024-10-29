40b54bb59f56cef308a59b86bc2dea9c79a7a576
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-29 16:00:09 +0100

    Paso 4 pero no funciona, revisar creacion de las factories etc de product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,5 +19,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         Category::factory(10)->create();
+        Product::factory(20)->create();
     }
 }
