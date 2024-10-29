7063d30aad2a5eb6269e1f5d22b8bdf3310884ae
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-10-29 17:00:15 +0100

    Paso 8 crear metodo update y destroy

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('lists/categories', [CategoryController::class, 'list']);
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'store']);
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('products', [ProductController::class, 'index']);
