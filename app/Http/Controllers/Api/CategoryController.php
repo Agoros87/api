06cf985daa49f4a0ebb705fca418b4dff3c86635
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-11-19 15:56:25 +0100

    Fin paso 13 creando comentarios para la documentacion y poner la scribe medio para k se puede autentificar

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,22 +8,38 @@ use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Knuckles\Scribe\Attributes\Endpoint;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @group Categories
+ * Managing categories
+ */
 class CategoryController extends Controller
 {
+    /**
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * Getting the list of the categories
+     */
     public function index()
     {
         abort_if(!auth()->user()->tokenCan('categories-list'),403);
         return CategoryResource::collection(Category::all());
     }
-
+    #[Endpoint('Show category' ,description: 'Get a specific category')]
     public function show(Category $category)
     {
         abort_if(!auth()->user()->tokenCan('categories-show'),403);
         return new CategoryResource($category);
     }
-
+    /**
+     * Store a new category
+     *
+     * Creating a new category
+     *
+     * @bodyParam name string required The name of the category. Example: Electronics
+     *
+     */
     public function store(StoreCategoryRequest $request)
     {
         $data = $request->all();
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,6 +7,12 @@ use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+/**
+ * @group Products
+ * Managing products
+ *
+ * @queryParam page int The page number. Example: 1
+ */
 class ProductController extends Controller
 {
 
